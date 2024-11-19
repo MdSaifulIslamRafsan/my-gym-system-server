b4245a33334c4036e2208373a34c76f2f4f0886f
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-11-19 22:35:19 +0600

    added mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import mongoose from "mongoose";
 import app from "./app"
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-})
+
+const connectedDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string);
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Example app listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error( "Database connection failed" , error)
+        process.exit(1)
+    }
+}
+export default connectedDB;
